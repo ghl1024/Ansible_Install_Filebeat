2907bd5e2f4af9f5932365ea78ba9e359230251a
Hayden <45088633+ghl1024@users.noreply.github.com>
2020-06-05 16:29:44 +0800

    Add some options

--- soft/filebeat/filebeat.sh
+++ soft/filebeat/filebeat.sh
@@ -8,14 +8,14 @@ if [ $Version -eq 7 ];then
    rpm -ivh filebeat-7.1.0-x86_64.rpm
    mv /etc/filebeat/filebeat.yml /etc/filebeat/fiilebeat.yml.bak
    mv /tmp/filebeat.yml /etc/filebeat/
-   systemctl start filebeat.service
+#   systemctl start filebeat.service
    systemctl enable filebeat.service 
 elif [ $Version -eq 6 ];then 
    cd /tmp
    rpm -ivh filebeat-7.1.0-x86_64.rpm
    mv /etc/filebeat/filebeat.yml /etc/filebeat/fiilebeat.yml.bak
    mv /tmp/filebeat.yml /etc/filebeat/
-   service filebeat start
+#   service filebeat start
    chkconfig --add filebeat
    chkconfig filebeat on        
 else
